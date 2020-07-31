f4966671db4c35df3598248ecfeaa2e7de02e091
James Paget <james.lostarkgames@gmail.com>
2020-07-31 22:30:58 +0100

    Adding experimental versions to new branch

--- 5across-Spacedogs.py
+++ 5across-Spacedogs.py
@@ -8,23 +8,30 @@ import math
 
 def solution(masses, locations):
 
+    ##BUBBLESORT (masses and locations) INEFFICIENT, BETTER WITH MERGE SORT
+    mItem = 0#Moving item
+    while True:
+        valSwitch = False#Check for any movement in a given cycle
+        for indDog in range(len(masses)):
+            if indDog+1 < len(masses):#If not overflowing
+                if masses[indDog] > masses[indDog+1]:
+                    mItem = masses.pop(indDog+1)
+                    masses.insert(indDog,mItem)
+                    mItem = locations.pop(indDog+1)
+                    locations.insert(indDog,mItem)
+                    valSwitch = True#movement occured in given cycle
+        if valSwitch == False:
+            break
+    ##BUBBLESORT
+    
     #Find dimensions
     dogDim = len( locations[0] ) #Number of coords for any given spacedog => number of dimensions
         
     while True:
         dist = 0
-        #print(masses)
-        #print(locations)
         #Find smallest dog
-        smallDog = 0#1st dog (0) as base smallest
-        for Dog in  range(len(masses)):
-            #print("Dog: ",Dog)
-            #print("smallDog test: ",smallDog)
-            if masses[Dog] < masses[smallDog]:#DOES NOT ACCOUNT FOR EQUAL MASSES OF DOG
-                smallDog = Dog
-                #print("Final: ",smallDog)
-            #print(" ")
-
+        smallDog = 0#1st (0th index) dog is always smallest (sorted)
+        
         #Find dog to fight ( find closest dog )
         Cdist = 0 #Current iterations distance
         Sdist = 99999999999999999999999999 #Shortest distance for given dog
@@ -52,15 +59,33 @@ def solution(masses, locations):
             Ccoord.append( math.floor(( locations[smallDog][coords] + locations[closeDog][coords] )/2 ) )
             Fcoords = Fcoords + tuple(Ccoord)
 
-        #CloseDog DNA
-        masses.insert(closeDog, newMass)
-        locations.insert(closeDog, Fcoords)
-        #Darwinism CloseDog
-        masses.pop(closeDog +1)
-        locations.pop(closeDog +1)
-        #Clean Genepool (of smallDog)
-        masses.pop(smallDog)
-        locations.pop(smallDog)
+        #Remove both dogs
+        masses.pop(closeDog)
+        masses.pop(smallDog)#Should always pop 0
+        locations.pop(closeDog)
+        locations.pop(smallDog)#Should always pop 0
+        #e.g [{a},b,c,{d},e,f,g] --> [b,c,e,f,g], checking up from (and including) e, index 2, counting 3 times (in terms of masses)
+
+        #Remove from list and re-place newDog (closeDog) into correct place in masses (ordered)
+        for indDog in range( len(masses) - closeDog+1 ):#Check all dogs after close dog, because mass only gets larger
+            upperCase = (newMass <= masses[indDog + closeDog-1])
+            lowerCase = (newMass >= masses[indDog + closeDog-2])
+            finalCase = upperCase and lowerCase
+            if closeDog == 1:
+                masses.insert(0,  newMass)
+                locations.insert(0, Fcoords)
+                break
+            if finalCase == True:#If it fits inbetween the masses
+                masses.insert((indDog + closeDog-1),    newMass)
+                locations.insert((indDog + closeDog-1),  Fcoords)
+                break
+            if indDog == len(masses) -1:#DOG AT END OF THE LINE
+                masses.append(newMass)
+                locations.append(Fcoords)
+                break
+        if len(masses) == 0:
+            masses.append( newMass)
+            locations.append(Fcoords)
 
         #Now loop cycle ( until one left )
         if len( masses ) == 1:
