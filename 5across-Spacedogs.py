1c126c30d07768350db449285a4c362db5a25f31
Certems <69006102+Certems@users.noreply.github.com>
2020-08-02 23:52:03 +0100

    Update 5across-Spacedogs.py

--- 5across-Spacedogs.py
+++ 5across-Spacedogs.py
@@ -8,19 +8,11 @@ import math
 def solution(masses, locations):
 
     #Sort dogs into ascending order
-    moveItem = 0
-    while True:
-        switched = False
-        for dogInd in range(len(masses)):
-            if dogInd +1 < len(masses):
-                if masses[dogInd +1] < masses[dogInd]:
-                    moveItem = masses.pop(dogInd +1)
-                    masses.insert(dogInd, moveItem)
-                    moveItem = locations.pop(dogInd +1)
-                    locations.insert(dogInd, moveItem)
-                    switched = True
-        if switched == False:
-            break
+    
+    sorted = merge_sort(masses, locations)
+    masses = sorted[0]
+    locations = sorted[1]
+    
     #Establish number of dimensions
     DimensionNumber = len(locations[0])
     #print("SortedMasses: ",masses)
@@ -35,7 +27,7 @@ def solution(masses, locations):
         #print("locationsStart: ",locations)
         
         #Find closest dog
-        coordMagFinLowest = 999999999999999999999999999999999999
+        coordMagFinLowest = 99999999999999999999999999
         closestDog = 0
         for dogInd in range(len(masses)):
             coordMag = 0
@@ -104,3 +96,90 @@ def solution(masses, locations):
     return coordTot
 
 #print ( solution(masses, locations) )
+
+
+
+
+
+
+
+
+def merge_sort(my_list, loc_list):
+        #Find the lowest larger power of 2 than the size of the list
+    log_2_len_u = math.ceil(math.log2(len(my_list)))
+    req_len = math.pow(2,log_2_len_u)
+    diff = int(req_len - len(my_list))
+
+    my_max = max(my_list)+1
+    for i in range(diff):
+        my_list.append(my_max)
+        loc_list.append(0)
+
+    group_size = 1
+    while(group_size < len(my_list)):
+            #Double the size of the subgroups
+        group_size = int(group_size)
+
+            #new_list is the list after sorting the subgroups
+        new_list = []
+        new_list_loc = []
+        ind_list = []
+            #For loop to find the sublists to consider
+        for i in range(math.floor(len(my_list)/(group_size*2))):
+            ind_list.append( 2*group_size*i )
+
+        for i in ind_list:
+            sub_1 = []
+            sub_2 = []
+            for j in range(group_size):
+                sub_1.append(my_list[i+j])
+                sub_2.append(my_list[i+group_size+j])
+                
+                sub_1_loc.append(loc_list[i+j])
+                sub_2_loc.append(loc_list[i+group_size+j])
+            #print("sub_1: ", sub_1)
+            #print("sub_2: ", sub_2)
+            #print("")
+            #print("")
+            
+            sorted_list_comp = pairwise_sorting(sub_1,sub_2,sub_1_loc,sub_2_loc)
+            sorted_list = sorted_list_comp[0]
+            sorted_list_loc = sorted_list_comp[1]
+
+            for value in sorted_list:
+                new_list.append( value )
+            for value in sorted_list_loc:
+                new_list_loc.append( value )
+        my_list = new_list
+        loc_list = new_list_loc
+        #print("my_list: ", my_list)
+        group_size *= 2
+    my_list = my_list[0:len(my_list)-diff]
+    loc_list = loc_list[0:len(loc_list)-diff]
+    #print("Final List: ", my_list)
+    
+    return [my_list, loc_list]
+    
+
+def pairwise_sorting(list1,list2,loc1,loc2):
+    return_list = []
+    return_list_loc = []
+    while (len(list1) != 0 and len(list2) != 0):
+        if (list1[0] <= list2[0]):
+            return_list.append( list1.pop(0) )
+            return_list_loc.append( loc1.pop(0) )
+        else:
+            return_list.append( list2.pop(0) )
+            return_list_loc.append( loc2.pop(0) )
+    if (len(list1) == 0):
+        for i in range(len(list2)):
+            value = list2[i]
+            return_list.append( value )
+            return_list_loc.append( loc2[i] )
+    else:
+        for i in range(len(list1)):
+            value = list1[i]
+            return_list.append( value )
+            return_list_loc.append( loc1[i] )
+    return [return_list,return_list_loc]
+
